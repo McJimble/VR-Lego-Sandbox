170e3db7c0cef12d712f15ed7f9a5e5df5e99f38
David Gleaton <hgleato@clemson.edu>
2021-03-11 17:23:03 -0500

    Added spawning of all 4 types, along with color switching. Has the dup object overlap problem

---
+++ VR Lego Sandbox/Assets/ColorButton.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorButton : MonoBehaviour
+{
+    public GameObject ColorSwatch;
+    public Material Red;
+    public Material Green;
+    public Material Blue;
+    private int change = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+
+            switch (change)
+            {
+                case 0:
+                    ColorSwatch.GetComponent<MeshRenderer>().material = Red;
+                    change++;
+                    break;
+
+                case 1:
+                    ColorSwatch.GetComponent<MeshRenderer>().material = Green;
+                    change++;
+                    break;
+            
+                case 2:
+                    ColorSwatch.GetComponent<MeshRenderer>().material = Blue;
+                    change = 0;
+                    break;
+            }
+            
+        }
+
+
+
+    }
+}
--- VR Lego Sandbox/Assets/Scripts/testspawn.cs
+++ VR Lego Sandbox/Assets/Scripts/testspawn.cs
@@ -7,14 +7,12 @@ public class testspawn : MonoBehaviour
     // Reference to the Prefab. Drag a Prefab into this field in the Inspector.
     public GameObject myPrefab;
     public GameObject player;
+    public GameObject ColorSwatch;
+
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Z))
-        {
-            //Instantiate(myPrefab, new Vector3(1, 1, 1), Quaternion.identity);
-            SpawnObject();
-        }
+
         
     }
 
@@ -22,9 +20,10 @@ public class testspawn : MonoBehaviour
     {
         // Old spawn code, changed below to always spawn where player is facing.
         //Instantiate(myPrefab, new Vector3(player.transform.position.x + 1, player.transform.position.y, player.transform.position.z + 1), transform.rotation);
-
         // Spawn in front of player by using the forward direction of the camera's transform and offset a bit.
         Instantiate(myPrefab, player.transform.position + (player.transform.forward * 3), Quaternion.identity);
+        //ColorSwatch.GetComponent<MeshRenderer>().material.GetColor
+        //myPrefab.GetComponent<LegoGroup>().ChangeGroupColor(ColorSwatch.GetComponent<Renderer>().material.color);
     }
 
 }
