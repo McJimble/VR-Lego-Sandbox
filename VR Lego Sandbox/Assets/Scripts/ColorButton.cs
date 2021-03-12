79476768f34bf4f0dec3ac3838dff525be8c924c
Jimmy Robertson <54756070+Jimbologist@users.noreply.github.com>
2021-03-12 02:27:30 -0500

    Milestone 2 build

--- VR Lego Sandbox/Assets/Scripts/ColorButton.cs
+++ VR Lego Sandbox/Assets/Scripts/ColorButton.cs
@@ -9,40 +9,50 @@ public class ColorButton : MonoBehaviour
     public Material Green;
     public Material Blue;
     private int change = 0;
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Hey Hamlin, just letting you know I made some changes and commented this out temporarily so that I could
+        // link the color swapping to some multiplexed controls through a function. I hope you don't mind, just wanted
+        // to show off what you took the time to implement for the second meeting!! You can change this back after it.
+
+        /*
         if (Input.GetKeyDown(KeyCode.C))
         {
 
-            switch (change)
-            {
-                case 0:
-                    ColorSwatch.GetComponent<MeshRenderer>().material.color = Red.color;
-                    change++;
-                    break;
-
-                case 1:
-                    ColorSwatch.GetComponent<MeshRenderer>().material.color = Green.color;
-                    change++;
-                    break;
-            
-                case 2:
-                    ColorSwatch.GetComponent<MeshRenderer>().material.color = Blue.color;
-                    change = 0;
-                    break;
-            }
+            SwapColor()
             
         }
 
+        */
 
+    }
 
+    public void SwapColor()
+    {
+        switch (change)
+        {
+            case 0:
+                ColorSwatch.GetComponent<MeshRenderer>().material.color = Red.color;
+                change++;
+                break;
+
+            case 1:
+                ColorSwatch.GetComponent<MeshRenderer>().material.color = Green.color;
+                change++;
+                break;
+
+            case 2:
+                ColorSwatch.GetComponent<MeshRenderer>().material.color = Blue.color;
+                change = 0;
+                break;
+        }
     }
 }
